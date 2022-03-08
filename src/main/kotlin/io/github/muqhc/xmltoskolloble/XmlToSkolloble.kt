b128d242ec00455933898293e3b2505040166bb4
muqhc <muqhc07@gmail.com>
2022-03-08 19:38:39 +0900

    Initial commit

---
+++ src/main/kotlin/io/github/muqhc/xmltoskolloble/XmlToSkolloble.kt
@@ -0,0 +1,38 @@
+package io.github.muqhc.xmltoskolloble
+
+import org.dom4j.Attribute
+import org.dom4j.Element
+import org.dom4j.Namespace
+import org.dom4j.io.SAXReader
+
+
+fun xmlToSkolloble(xml: String): String = xmlToSkolloble(SAXReader().read(xml.byteInputStream()).rootElement)
+
+fun xmlToSkolloble(element: Element, prefix: String = "", startP: String = prefix): String =
+    if (element.elements().isEmpty() && element.text.isEmpty()) "$startP${genElementTag(element)} \\"
+    else if (element.isTextOnly)
+        if (element.text.split("\n").count() == 1) "$startP${genElementTag(element)} - \"${element.text}\""
+        else """
+           |${genElementTag(element)} {${element.text.split("\n").joinToString("\n","\n") { "$prefix\"$it\"" }}
+           |}
+        """.trimMargin()
+    else if (element.isRootElement) "${genElementTag(element)}/ ${element.elements().joinToString("\n","\n") { xmlToSkolloble(it as Element, "$prefix    ") }}"
+    else if (element.elements().count() == 1) "$startP${genElementTag(element)} - ${xmlToSkolloble(element.elements().first() as Element,prefix, "")}"
+    else """
+           |$startP${genElementTag(element)} {${element.elements().joinToString("\n","\n") { xmlToSkolloble(it as Element, "$prefix    ") }}
+           |$prefix}
+        """.trimMargin()
+
+fun genElementTag(element: Element): String =
+    "${element.name}${if (element.namespace.name != null) "@"+element.namespace.name else ""}" +
+            if (genElementAttr(element).isNotEmpty()) ": "+genElementAttr(element) else ""
+
+fun genElementAttr(element: Element): String =
+    ((if (element.attributeCount() == 0) emptyList()
+    else (element.declaredNamespaces() as List<Namespace>).map {
+        (if (it.prefix.isNotEmpty()) it.prefix+"@" else "")+"xmlns\"${it.uri}\""
+    }) +
+    (if (element.attributeCount() == 0) emptyList()
+    else (element.attributes() as List<Attribute>).map {
+        it.name+(if (it.namespace != null) "@"+it.namespace.prefix else "")+"\"${it.value}\""
+    })).joinToString(" ")
---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,85 @@
+import io.github.muqhc.xmltoskolloble.xmlToSkolloble
+import org.dom4j.io.SAXReader
+
+class PrintField {
+    operator fun <T> T.unaryMinus() = this.also(::println)
+}
+
+fun println(init: PrintField.() -> Unit) = PrintField().let(init)
+
+fun main() = println {
+    val _xml = """
+        |<?xml version="1.0" encoding="UTF-8"?>
+        |<project xmlns="http://maven.apache.org/POM/4.0.0"
+        |         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        |         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+        |    <modelVersion>4.0.0</modelVersion>
+        |
+        |    <groupId>io.github.muqhc</groupId>
+        |    <artifactId>sample</artifactId>
+        |    <version>1.0.0</version>
+        |
+        |    <properties>
+        |        <kotlin.version>1.6.10</kotlin.version>
+        |        <main.class>io.github.muqhc.sample.Sample</main.class>
+        |    </properties>
+        |
+        |    <dependencies>
+        |        <dependency>
+        |            <groupId>org.jetbrains.kotlin</groupId>
+        |            <artifactId>kotlin-stdlib</artifactId>
+        |            <version>${"$"}{kotlin.version}</version>
+        |        </dependency>
+        |        <dependency>
+        |            <groupId>org.jetbrains.kotlin</groupId>
+        |            <artifactId>kotlin-test-junit</artifactId>
+        |            <version>${"$"}{kotlin.version}</version>
+        |            <scope>test</scope>
+        |        </dependency>
+        |    </dependencies>
+        |
+        |    <build>
+        |        <sourceDirectory>${"$"}{project.basedir}/src/main/kotlin</sourceDirectory>
+        |        <testSourceDirectory>${"$"}{project.basedir}/src/test/kotlin</testSourceDirectory>
+        |        <plugins>
+        |            <plugin>
+        |                <groupId>org.jetbrains.kotlin</groupId>
+        |                <artifactId>kotlin-maven-plugin</artifactId>
+        |                <version>${"$"}{kotlin.version}</version>
+        |
+        |                <executions>
+        |                    <execution>
+        |                        <id>compile</id>
+        |                        <goals>
+        |                            <goal>compile</goal>
+        |                        </goals>
+        |                    </execution>
+        |
+        |                    <execution>
+        |                        <id>test-compile</id>
+        |                        <goals>
+        |                            <goal>test-compile</goal>
+        |                        </goals>
+        |                    </execution>
+        |                </executions>
+        |            </plugin>
+        |            <plugin>
+        |                <groupId>org.apache.maven.plugins</groupId>
+        |                <artifactId>maven-jar-plugin</artifactId>
+        |                <version>2.6</version>
+        |                <configuration>
+        |                    <archive>
+        |                        <manifest>
+        |                            <addClasspath>true</addClasspath>
+        |                            <mainClass>${"$"}{main.class}</mainClass>
+        |                        </manifest>
+        |                    </archive>
+        |                </configuration>
+        |            </plugin>
+        |        </plugins>
+        |    </build>
+        |</project>
+        |""".trimMargin()
+    -xmlToSkolloble(_xml)
+
+}
