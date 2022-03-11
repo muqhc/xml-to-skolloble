31f15daf33fffcdaa2fc82b6f02ba5ab2397d5fc
muqhc <muqhc07@gmail.com>
2022-03-11 16:35:50 +0900

    improve test

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,78 +1,244 @@
 import io.github.muqhc.xmltoskolloble.xmlToSkolloble
-import org.dom4j.io.SAXReader
 
 fun main() {
-    val _xml = """
-        |<?xml version="1.0" encoding="UTF-8"?>
-        |<project xmlns="http://maven.apache.org/POM/4.0.0"
-        |         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        |         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-        |    <modelVersion>4.0.0</modelVersion>
-        |
-        |    <groupId>io.github.muqhc</groupId>
-        |    <artifactId>sample</artifactId>
-        |    <version>1.0.0</version>
-        |
-        |    <properties>
-        |        <kotlin.version>1.6.10</kotlin.version>
-        |        <main.class>io.github.muqhc.sample.Sample</main.class>
-        |    </properties>
-        |
-        |    <dependencies>
-        |        <dependency>
-        |            <groupId>org.jetbrains.kotlin</groupId>
-        |            <artifactId>kotlin-stdlib</artifactId>
-        |            <version>${"$"}{kotlin.version}</version>
-        |        </dependency>
-        |        <dependency>
-        |            <groupId>org.jetbrains.kotlin</groupId>
-        |            <artifactId>kotlin-test-junit</artifactId>
-        |            <version>${"$"}{kotlin.version}</version>
-        |            <scope>test</scope>
-        |        </dependency>
-        |    </dependencies>
-        |
-        |    <build>
-        |        <sourceDirectory>${"$"}{project.basedir}/src/main/kotlin</sourceDirectory>
-        |        <testSourceDirectory>${"$"}{project.basedir}/src/test/kotlin</testSourceDirectory>
-        |        <plugins>
-        |            <plugin>
-        |                <groupId>org.jetbrains.kotlin</groupId>
-        |                <artifactId>kotlin-maven-plugin</artifactId>
-        |                <version>${"$"}{kotlin.version}</version>
-        |
-        |                <executions>
-        |                    <execution>
-        |                        <id>compile</id>
-        |                        <goals>
-        |                            <goal>compile</goal>
-        |                        </goals>
-        |                    </execution>
-        |
-        |                    <execution>
-        |                        <id>test-compile</id>
-        |                        <goals>
-        |                            <goal>test-compile</goal>
-        |                        </goals>
-        |                    </execution>
-        |                </executions>
-        |            </plugin>
-        |            <plugin>
-        |                <groupId>org.apache.maven.plugins</groupId>
-        |                <artifactId>maven-jar-plugin</artifactId>
-        |                <version>2.6</version>
-        |                <configuration>
-        |                    <archive>
-        |                        <manifest>
-        |                            <addClasspath>true</addClasspath>
-        |                            <mainClass>${"$"}{main.class}</mainClass>
-        |                        </manifest>
-        |                    </archive>
-        |                </configuration>
-        |            </plugin>
-        |        </plugins>
-        |    </build>
-        |</project>
-        |""".trimMargin()
+    val _xml = """<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    
+<meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8" />
+    
+<title>frogui-main</title><link href="images/logo-icon.svg" rel="icon" type="image/svg" />
+<script>var pathToRoot = "";</script>
+<script type="text/javascript" src="scripts/sourceset_dependencies.js" async="async"></script><link href="styles/style.css" rel="Stylesheet" /><link href="styles/logo-styles.css" rel="Stylesheet" /><link href="styles/jetbrains-mono.css" rel="Stylesheet" /><link href="styles/main.css" rel="Stylesheet" />
+<script type="text/javascript" src="scripts/clipboard.js" async="async"></script>
+<script type="text/javascript" src="scripts/navigation-loader.js" async="async"></script>
+<script type="text/javascript" src="scripts/platform-content-handler.js" async="async"></script>
+<script type="text/javascript" src="scripts/main.js" async="async"></script>  </head>
+  
+<body>
+    
+<div id="container">
+      
+<div id="leftColumn"><a href="index.html">
+          
+<div id="logo" />
+        </a>
+        
+<div id="paneSearch" />
+        
+<div id="sideMenu" />
+      </div>
+      
+<div id="main">
+        
+<div id="leftToggler"><span class="icon-toggler" /></div>
+<script type="text/javascript" src="scripts/main.js"></script>        
+<div class="main-content" id="content" pageids="frogui-main::////PointingToDeclaration//348893187">
+          
+<div class="navigation-wrapper" id="navigation-wrapper">
+            
+<div class="breadcrumbs"><a href="index.html">frogui-main</a></div>
+            
+<div class="pull-right d-flex">
+              
+<div id="searchBar" />
+            </div>
+          </div>
+          
+<div class="cover ">
+            
+<h1 class="cover"><span>frogui-main</span></h1>
+          </div>
+          
+<h2 class="">Packages</h2>
+          
+<div class="table"><a data-name="472877031%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui" id="472877031%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui/index.html">io.github.muqhc.frogui</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="472877031%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-1785183148%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.builder" id="-1785183148%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.builder/index.html">io.github.muqhc.frogui.builder</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-1785183148%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-849236502%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.builder.panel" id="-849236502%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.builder.panel/index.html">io.github.muqhc.frogui.builder.panel</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-849236502%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="579602363%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.builder.panel.group" id="579602363%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.builder.panel.group/index.html">io.github.muqhc.frogui.builder.panel.group</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="579602363%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-482111443%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.node" id="-482111443%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.node/index.html">io.github.muqhc.frogui.node</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-482111443%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-1302791620%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.node.component" id="-1302791620%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.node.component/index.html">io.github.muqhc.frogui.node.component</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-1302791620%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="313284050%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.node.component.panel" id="313284050%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.node.component.panel/index.html">io.github.muqhc.frogui.node.component.panel</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="313284050%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-600200689%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.node.layout" id="-600200689%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.node.layout/index.html">io.github.muqhc.frogui.node.layout</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-600200689%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-1145203680%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.node.layout.group" id="-1145203680%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.node.layout.group/index.html">io.github.muqhc.frogui.node.layout.group</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-1145203680%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div><a data-name="-69709267%2FPackages%2F348893187" anchor-label="io.github.muqhc.frogui.util" id="-69709267%2FPackages%2F348893187" data-filterable-set=":frogui-main:dokkaHtml/main" />
+            
+<div class="table-row" data-filterable-current=":frogui-main:dokkaHtml/main" data-filterable-set=":frogui-main:dokkaHtml/main">
+              
+<div>
+                
+<div class="main-subrow ">
+                  
+<div class=""><span class="inline-flex"><a href="frogui-main/io.github.muqhc.frogui.util/index.html">io.github.muqhc.frogui.util</a><span class="anchor-wrapper"><span class="anchor-icon" pointing-to="-69709267%2FPackages%2F348893187" />
+                        
+<div class="copy-popup-wrapper "><span class="copy-popup-icon" /><span>Link copied to clipboard</span></div>
+                      </span></span></div>
+                  
+<div class="pull-right" />
+                </div>
+                
+<div />
+              </div>
+            </div>
+          </div>
+        </div>
+        
+<div class="footer"><span class="go-to-top-icon"><a href="#content" /></span><span>© 2022 Copyright</span><span class="pull-right"><span>Generated by </span><a href="https://github.com/Kotlin/dokka"><span>dokka</span><span class="padded-icon" /></a></span></div>
+      </div>
+    </div>
+  
+</body></html>"""
     xmlToSkolloble(_xml).let(::println)
 }
